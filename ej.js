9428cc45b0070f8a1f8c550b84411182d0c7ced2
Carlos A Chavez Perez <hamandatorres@gmail.com>
2023-03-01 01:34:02 -0700

    learning
    session with ej

--- ej.js
+++ ej.js
@@ -1,1 +1,13 @@
-console.log(1 == "1");
+let result = 1;
+let counter = 0;
+while (counter < 10) {
+	result = result * 2;
+	counter = counter + 1;
+}
+console.log(result);
+
+let yourName;
+do {
+	yourName = prompt("Who are you?");
+} while (!yourName);
+console.log(yourName);
