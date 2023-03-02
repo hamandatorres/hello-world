0d05a465c8e0a3af8c293c15a81c1b870afd916c
Carlos A Chavez Perez <hamandatorres@gmail.com>
2023-03-02 15:58:25 -0700

    study
    study session 3.02.2023

--- ej.js
+++ ej.js
@@ -1,13 +1,7 @@
-let result = 1;
-let counter = 0;
-while (counter < 10) {
-	result = result * 2;
-	counter = counter + 1;
+function chicken() {
+	return egg();
 }
-console.log(result);
-
-let yourName;
-do {
-	yourName = prompt("Who are you?");
-} while (!yourName);
-console.log(yourName);
+function egg() {
+	return chicken();
+}
+console.log(chicken() + " came first.");
