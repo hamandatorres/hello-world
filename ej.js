0008fa465a959e08525dd7fb1b336d062a2c81ec
Carlos A Chavez Perez <hamandatorres@gmail.com>
2023-03-07 16:29:04 -0700

    study session
    study session eloquent javascript 3.07

--- ej.js
+++ ej.js
@@ -1,1 +1,5 @@
-console.log(null.length);
+console.log(String(1).padStart(4, "0"));
+let sentence = "Secretarybirds specialize in stomping";
+let words = sentence.split(" ");
+console.log(words);
+console.log(words.join(" "));
